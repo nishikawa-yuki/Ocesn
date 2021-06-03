cf9cda5633bf913d3bc5d6391b1613f582a473cb
西川勇気 <sussy168168@gmail.com>
2021-06-03 10:04:54 +0900

    h2.cssに追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,11 +11,6 @@ $('.slider').slick({
     dots: true,//下部ドットナビゲーションの表示
 });
 
-
-
-
-
-
 //toppageへのボタン
 
 //スクロールした際の動きを関数でまとめる
@@ -54,36 +49,32 @@ $('#page-top').click(function () {
             $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
         });	
 	}
-
-
-  let arr = []
-    //初期値の設定
-    function TypingInit() {
-      $('#asia').each(function (i) { //js_typingクラスを全て処理をおこなう
-        arr[i] = new ShuffleText(this);//動作させるテキストを配列に格納
-      });
-    }
-    //スクロールした際のアニメーションの設定
-    function TypingAnime() {
-      $("#asia").each(function (i) {
-        var elemPos = $(this).offset().top - 50;//要素より、50px上の
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll >= elemPos - windowHeight) {
-          if(!$(this).hasClass("endAnime")){//endAnimeのクラスがあるかチェック
-            arr[i].start();//配列で登録テキストのアニメーションをおこなう
-            arr[i].duration = 800;//テキストが最終変化するまでの時間※規定値600
-            $(this).addClass("endAnime");//１度アニメーションした場合はendAnimeクラスを追加
-          }
-        }else{
-          $(this).removeClass("endAnime"); //範囲外にスクロールした場合はendAnimeのクラスを削除
-        }
-      });
-    }
-    
-    // 画面をスクロールをしたら動かしたい場合の記述
-    $(window).scroll(function () {
-      TypingAnime();/* アニメーション用の関数を呼ぶ*/
-    });// ここまで画面をスクロールをしたら動かしたい場合の記述
     return false;//リンク自体の無効化 
 });
+
+
+function slideAnime(){
+	//====上に動くアニメーションここから===
+		$('.upAnime').each(function(){
+            var elemPos = $(this).offset().top-50;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll >= elemPos - windowHeight){
+            // 上から下へ表示するクラスを付与
+            // テキスト要素を挟む親要素（上）とテキスト要素を元位置でアニメーションをおこなう
+            $(this).addClass("slideAnimeUpDown");
+            // 要素を上枠外に移動しCSS アニメーションで上から元の位置に移動
+            $(this).children(".upAnimeInner").addClass("slideAnimeDownUp");
+            // 子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+            }else{
+            // 上から下へ表示するクラスを取り除く
+            $(this).removeClass("slideAnimeUpDown");
+            $(this).children(".upAnimeInner").removeClass("slideAnimeDownUp");
+            }
+            });
+	}
+	
+	// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		slideAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
